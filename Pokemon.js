cca6eedbe5b28758193765bafb2e259138d1f906
Oreff <emmasanyaolu1@gmail.com>
2025-04-01 22:50:54 -0300

    Basic Functionalities Like Add Pokemon Works

--- Pokemon.js
+++ Pokemon.js
@@ -1,2 +1,46 @@
 const submit = document.getElementById("submit");
-const pokemonName = document.getElementById("pokemon-name")
+const getPokemonName = document.getElementById("pokemon-name");
+
+let initialUrl = "https://pokeapi.co/api/v2/pokemon/";
+let url;
+async function getName(event){
+    event.preventDefault();
+    let pokemonName = getPokemonName.value.trim();
+    url = initialUrl + pokemonName;
+    getPokemonName.value = "";
+    let f = await getDetails(url);
+    displayDetails(f)
+}
+
+
+async function getDetails(url) {
+    try{
+        let data = await fetch(url);
+        let jsonFormat = await data.json();
+        return jsonFormat;
+    }
+    catch(error){
+        console.error(error)
+    }
+}
+
+function displayDetails(object){
+    console.log(object.name)
+    console.log(object.sprites.front_default)
+    console.log(object)
+    //Variables
+    let details = document.createElement("div");
+    let pokemonImage = document.createElement("img")
+    let pokemonName = document.createElement("p")
+    pokemonImage.src = object.sprites.front_default;
+    pokemonName.textContent = object.name
+    details.append(pokemonImage , pokemonName)
+    document.body.append(details)
+    
+}
+
+// const getData = new Promise((resolve, reject)  =>{
+
+// })
+
+submit.addEventListener("click" , getName)
